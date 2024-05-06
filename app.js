94d20679816ca225a32e0d8cb572196582bd4371
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-05-06 12:41:16 +0300

    lesson 2

---
+++ app.js
@@ -0,0 +1,98 @@
+// try {
+//     console.log(a);
+//     let a = 2 + 5;
+// } catch (err) {
+//     console.log(err.massage);
+// }
+
+// //  let arr = delta => () {
+
+// // }
+
+//masalalarr... 
+// 1masala
+// function sum(x,y) {
+//     console.log(x + y);
+// }
+// sum(2,3)
+
+// 2masala
+// function toSekunds(minutes) {
+//     console.log(minutes * 60);
+// }
+// toSekunds(5)
+
+// 3masala
+// function nextInteger(int) {
+//     console.log(int + 1);
+// }
+// nextInteger(2)
+
+// 4masala
+// function uchburchakYuzi(asos, balandlik) {
+//     console.log((asos * balandlik) / 2);
+// }
+// uchburchakYuzi(7, 4)
+
+// 5masala  
+// function ageToDays(age) {
+//     // code...
+//     console.log(age * 365);
+// }
+// ageToDays(65)
+
+// 6masala
+// function daraja(x){
+//     console.log(x**3);
+// }
+// daraja(5)
+
+// 7masala
+// function firstElement(arr) {
+//     arr.forEach((el) => {
+//         el = Number
+//             console.log(el);
+
+//     });
+// }
+
+// firstElement([1,2,3])
+
+// 8masala
+// function power(x,y) {
+// console.log(x * y);
+// }
+// power(10, 3)
+
+// 9masala
+// function hourToSekunds(hour) {
+//      let minut = 60;
+//      let sekund = 60;
+//      console.log(hour * minut * sekund);    
+// }
+// hourToSekunds(2)
+
+// 10masala
+// function uchinchiTomon(tomon1, tomon2) {
+//     console.log((tomon1 + tomon2) - 1);
+// }
+// uchinchiTomon(8,10)
+
+// 11masala
+// function qoldiq(son1, son2) {
+//     console.log(son1 % son2);
+// }
+// qoldiq(3, 2)
+
+// 12masala
+// function turtburchakYuzi(boyi, eni) {
+//     console.log(boyi * eni);
+// }
+// turtburchakYuzi(2,9)
+
+// 13masala
+function stringQoshish(a) {
+    a = "something";
+}
+console.log(string);
+ let string = stringQoshish( "a" + " " + " is better than nothing")
---
+++ note.md
@@ -0,0 +1,11 @@
+...function konstruktor
+...function factory
+... arow function
+this
+key sestive
+ 3lik - turnery operator
+ function parametrs
+
+spret - ...argument 
+spret vs rest operatorlari
+heap & stack
